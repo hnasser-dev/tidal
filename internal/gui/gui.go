462b0186d0b7f31099dcd6bf2cc80bc0fb5b70f1
hnasser-dev <hnasser.dev@proton.me>
2025-05-12 20:50:00 +0100

    removed clipboard from struct and removed comments from previous commit

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -11,7 +11,6 @@ import (
 type GuiWrapper struct {
 	App           fyne.App
 	PrimaryWindow fyne.Window
-	Clipboard     fyne.Clipboard
 }
 
 var Gui *GuiWrapper
@@ -28,7 +27,6 @@ func init() {
 	Gui = &GuiWrapper{
 		App:           a,
 		PrimaryWindow: primaryWindow,
-		Clipboard:     a.Clipboard(),
 	}
 
 	menuMap := map[string]func() *fyne.Container{
