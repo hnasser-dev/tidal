6168080ddf6715a432065071ad8b633d0bdeb4a8
hnasser-dev <hnasser.dev@proton.me>
2025-05-24 10:01:47 +0100

    working on updateFrequency logic

--- internal/gui/subsection_title_setup.go
+++ internal/gui/subsection_title_setup.go
@@ -1,6 +1,9 @@
 package gui
 
 import (
+	"log"
+	"strconv"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
@@ -12,9 +15,40 @@ var titleSetupWindowSize fyne.Size = fyne.NewSize(600, 1) // height 1 lets the l
 func (g *GuiWrapper) getTitleSetupSubsection() *fyne.Container {
 
 	saveBtn := widget.NewButton("Save", nil)
+
 	titleTemplateEntry := getMultilineEntry("", saveBtn, 6)
+
 	tipLabel := widget.NewLabel(`You can use any Variables in your title template
 Access them using {{VariableName}}`)
+
+	updateFrequencyDigitEntry := widget.NewEntry()
+	updateFrequencyTimePeriodSelector := widget.NewSelect([]string{"Seconds", "Minutes"}, nil)
+
+	// TODO - make a function that handles the calculation to seconds
+	// call the function when either of these widgets is unchanged
+	// if it fails to parse - show a warning label or something
+	updateFrequencyDigitEntry.OnChanged = func(s string) {
+		digitAsInt, err := strconv.Atoi(s)
+		if err != nil {
+			saveBtn.Disable()
+			return
+		}
+		saveBtn.Enable()
+		log.Println(intVal)
+	}
+
+	updateFrequencyTimePeriodSelector.OnChanged = func(s string) {
+		if s != "" {
+			saveBtn.Enable()
+		} else {
+			saveBtn.Disable()
+		}
+	}
+
+	updateFrequencyContainer := container.New(
+		layout.NewHBoxLayout(),
+		updateFrequencyDigitEntry, updateFrequencyTimePeriodSelector,
+	)
 
 	return container.New(
 		layout.NewFormLayout(),
@@ -22,6 +56,10 @@ Access them using {{VariableName}}`)
 		titleTemplateEntry,
 		layout.NewSpacer(),
 		tipLabel,
+		widget.NewLabel("Update every "),
+		updateFrequencyContainer,
+		horizontalSpacer(3),
+		layout.NewSpacer(),
 		layout.NewSpacer(),
 		saveBtn,
 	)
